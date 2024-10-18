528bc7b375fb568b53ea208e5eecbfb7c0ab22cf
victor schmoegel <victorschmoegel@gmail.com>
2024-10-18 09:36:45 -0300

    Criado modelo para colaboradores

---
+++ api/model/colab.model.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const ColabSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 3
+  },
+  cpf: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 11
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 5
+  },
+  location: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 3
+  },
+}, { timestamps: true });
+
+const Colab = mongoose.model('Colab', ColabSchema);
+
+module.exports = Colab;
