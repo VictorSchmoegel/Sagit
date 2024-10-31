dc1d0c8bdf46437f4ce4945ad3fd9885477ab296
victor schmoegel <victorschmoegel@gmail.com>
2024-10-31 10:12:47 -0300

    adicionada funcionalidade de busca na página Home

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { fetchProjectsStart, fetchProjectsSuccess, fetchProjectsFailure } from "../redux/projectsSlice";
 import { fetchColabsStart, fetchColabsSuccess, fetchColabsFailure } from "../redux/colabSlice";
@@ -9,6 +9,8 @@ export default function Home() {
   const { colabs } = useSelector((state) => state.colabs);
   const navigate = useNavigate();
   const dispatch = useDispatch();
+
+  const [searchTerm, setSearchTerm] = useState('');
 
 
   useEffect(() => {
@@ -51,79 +53,95 @@ export default function Home() {
     }
   };
 
+  const filteredColabs = colabs.filter((colab) =>
+    colab.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    colab.cpf.includes(searchTerm) ||
+    colab.rg.includes(searchTerm)
+  );
+
   return (
     <main className='flex flex-col bg-slate-100 min-h-screen'>
-      <div>
-        <div>
-          <nav className="w-full bg-slate-600">
-            <ul className="flex gap-4 justify-around border p-3">
-              <li>
-                <select className="cursor-pointer" onChange={handleSelectChange}>
-                  <option value="PROJETOS">PROJETOS</option>
-                  {projects.map((project) => (
-                    <option key={project._id} value={project._id}>
-                      {project.name}
-                    </option>
-                  ))}
-                </select>
-              </li>
-              <Link to='/documents'>
-                <li className="cursor-pointer text-white">
-                  GERENCIAR DOCUMENTOS
-                </li>
-              </Link>
-              <Link to='/projetos'>
-                <li className="cursor-pointer text-white">
-                  GERENCIAR PROJETOS
-                </li>
-              </Link>
-            </ul>
-          </nav>
-          <section>
-            <div className="border p-4 my-2">
-              <h1 className="text-3xl text-center p-2">Colaboradores</h1>
-              <table className="min-w-full bg-white shadow-md rounded-lg mt-5">
-                <thead>
-                  <tr>
-                    <th className="px-4 py-2 border">NOME</th>
-                    <th className="px-4 py-2 border">CPF</th>
-                    <th className="px-4 py-2 border">RG</th>
-                    <th className="px-4 py-2 border">VISUALIZAR</th>
-                    <th className="px-4 py-2 border">DESMOBILIZAR</th>
+    <div>
+      <nav className="w-full bg-slate-600">
+        <ul className="flex gap-4 justify-around border p-3">
+          <li>
+            <select className="cursor-pointer" onChange={handleSelectChange}>
+              <option value="PROJETOS">PROJETOS</option>
+              {projects.map((project) => (
+                <option key={project._id} value={project._id}>
+                  {project.name}
+                </option>
+              ))}
+            </select>
+          </li>
+          <Link to='/documents'>
+            <li className="cursor-pointer text-white">
+              GERENCIAR DOCUMENTOS
+            </li>
+          </Link>
+          <Link to='/projetos'>
+            <li className="cursor-pointer text-white">
+              GERENCIAR PROJETOS
+            </li>
+          </Link>
+        </ul>
+      </nav>
+      <section>
+        <div className="border p-4 my-2">
+          <h1 className="text-3xl text-center p-2">Colaboradores</h1>
+          {/* Search input */}
+          <div className="w-full max-w-lg mx-auto mb-4">
+            <input
+              type="text"
+              placeholder="Buscar por nome, CPF ou RG"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              className="w-full p-2 border border-gray-300 rounded-lg"
+            />
+          </div>
+          <table className="min-w-full bg-white shadow-md rounded-lg mt-5">
+            <thead>
+              <tr>
+                <th className="px-4 py-2 border">NOME</th>
+                <th className="px-4 py-2 border">CPF</th>
+                <th className="px-4 py-2 border">RG</th>
+                <th className="px-4 py-2 border">VISUALIZAR</th>
+                <th className="px-4 py-2 border">DESMOBILIZAR</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredColabs.length > 0 ? (
+                filteredColabs.map((colab) => (
+                  <tr key={colab._id}>
+                    <td className="px-4 py-2 border text-start">{colab.name}</td>
+                    <td className="px-4 py-2 border text-center">{colab.cpf}</td>
+                    <td className="px-4 py-2 border text-center">{colab.rg}</td>
+                    <td className="px-4 py-2 border text-center">
+                      <Link to={`/colabs/${colab._id}`}>
+                        <button className="bg-blue-500 text-white px-3 py-2 rounded">
+                          Visualizar
+                        </button>
+                      </Link>
+                    </td>
+                    <td className="px-4 py-2 border text-center">
+                      <button className="bg-red-500 text-white px-3 py-2 rounded">
+                        Desmobilizar
+                      </button>
+                    </td>
                   </tr>
-                </thead>
-                <tbody>
-                  {Array.isArray(colabs) && colabs.length > 0 ? (
-                    colabs.map((colab) => (
-                      <tr key={colab._id}>
-                        <td className="px-4 py-2 border text-start">{colab.name}</td>
-                        <td className="px-4 py-2 border text-center">{colab.cpf}</td>
-                        <td className="px-4 py-2 border text-center">{colab.rg}</td>
-                        <td className="px-4 py-2 border text-center">
-                          <Link to={`/colabs/${colab._id}`}>
-                            <button className="bg-blue-500 text-white px-3 py-2 rounded">
-                              Visualizar
-                            </button>
-                          </Link>
-                        </td>
-                        <td className="px-4 py-2 border text-center">
-                          <button className="bg-red-500 text-white px-3 py-2 rounded">
-                            Desmobilizar
-                          </button>
-                        </td>
-                      </tr>
-                    ))
-                  ) : (
-                    <div>
-                      <p colSpan="5" className="text-center px-4 py-2 border">Nenhum colaborador encontrado</p>
-                    </div>
-                  )}
-                </tbody>
-              </table>
-            </div>
-          </section>
+                ))
+              ) : (
+                <tr>
+                  <td colSpan="5" className="text-center px-4 py-2 border">
+                    Nenhum colaborador encontrado
+                  </td>
+                </tr>
+              )}
+            </tbody>
+          </table>
         </div>
-      </div>
-    </main>
+      </section>
+    </div>
+  </main>
   )
 }
